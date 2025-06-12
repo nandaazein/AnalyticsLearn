4b9a86ea5206886103a0107981695fdf66c8782d
Srefaldi <2110131110004@mhs.ulm.ac.id>
2025-06-13 01:29:43 +0800

    test

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -10,7 +10,7 @@ const Homepage = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [nis, setNis] = useState(null);
   const [error, setError] = useState(null);
-
+  // ok
   useEffect(() => {
     const userData = localStorage.getItem("user");
     if (userData) {
@@ -32,36 +32,40 @@ const Homepage = () => {
     <>
       <Navbar />
       <div className="homepage flex flex-col min-h-screen pb-10 text-[#214C1E]">
-        <div className="max-w-6xl mx-auto px-8 text-center flex-grow bg-gray-100">
+        <div className="flex-grow max-w-6xl px-8 mx-auto text-center bg-gray-100">
           <h1 className="text-3xl font-bold my-8 text-[#1D6F35]">
             MEDIA PEMBELAJARAN ANALISIS DATA
           </h1>
-          <div className="grid md:grid-cols-3 grid-cols-1 gap-6">
-            <div className="box bg-white shadow-lg p-6 rounded-lg w-80 mx-auto border border-gray-300">
-              <img src={informasi} alt="Daftar Isi" className="mx-auto mb-4 w-52" />
+          <div className="grid grid-cols-1 gap-6 md:grid-cols-3">
+            <div className="p-6 mx-auto bg-white border border-gray-300 rounded-lg shadow-lg box w-80">
+              <img
+                src={informasi}
+                alt="Daftar Isi"
+                className="mx-auto mb-4 w-52"
+              />
               <h2 className="text-xl font-semibold">INFORMASI MEDIA</h2>
               <button
-                onClick={() => window.location.href = "/informasi"}
+                onClick={() => (window.location.href = "/informasi")}
                 className="mt-4 inline-block px-4 py-2 border border-[#1D6F35] text-[#1D6F35] rounded-md hover:bg-[#1D6F35] hover:text-white transition text-md cursor-pointer"
               >
                 LIHAT INFORMASI
               </button>
             </div>
-            <div className="box bg-white shadow-lg p-6 rounded-lg w-80 mx-auto border border-gray-300">
+            <div className="p-6 mx-auto bg-white border border-gray-300 rounded-lg shadow-lg box w-80">
               <img src={materi} alt="Materi" className="mx-auto mb-4 w-52" />
               <h2 className="text-xl font-semibold">MATERI</h2>
               <button
-                onClick={() => window.location.href = "/pengantar-bab"}
+                onClick={() => (window.location.href = "/pengantar-bab")}
                 className="mt-4 inline-block px-4 py-2 bg-[#1D6F35] text-white rounded-md hover:bg-[#155A2A] transition text-md cursor-pointer"
               >
                 MULAI BELAJAR
               </button>
             </div>
-            <div className="box bg-white shadow-lg p-6 rounded-lg w-80 mx-auto border border-gray-300">
+            <div className="p-6 mx-auto bg-white border border-gray-300 rounded-lg shadow-lg box w-80">
               <img src={siswa} alt="Informasi" className="mx-auto mb-3 w-52" />
               <h2 className="text-xl font-semibold">INFORMASI SISWA</h2>
               <button
-                onClick={() => window.location.href = "/dashboard-siswa"}
+                onClick={() => (window.location.href = "/dashboard-siswa")}
                 className="mt-4 inline-block px-4 py-2 border border-[#1D6F35] text-[#1D6F35] rounded-md hover:bg-[#1D6F35] hover:text-white transition text-md cursor-pointer"
               >
                 LIHAT INFORMASI SISWA
